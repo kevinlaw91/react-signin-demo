33dd2ae5a746a6765712add27607136e2f7ec52c
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2024-06-16 05:52:29 +0800

    Added dep react-helmet-async

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { HelmetProvider } from 'react-helmet-async';
 import router from '@/router.tsx';
 import '@/index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HelmetProvider>
+      <RouterProvider router={router} />
+    </HelmetProvider>
   </React.StrictMode>,
 );
