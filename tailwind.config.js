49452abbab99a0bf2858bc2137fb16f836ed5d26
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2024-06-16 03:57:55 +0800

    Add primary theme color to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#BD5B3E',
+          50: '#F0D7D0',
+          100: '#E9C5BB',
+          200: '#DBA190',
+          300: '#CD7E66',
+          400: '#BD5B3E',
+          500: '#934730',
+          600: '#693222',
+          700: '#3E1E14',
+          800: '#140A07',
+          900: '#0b0606',
+          950: '#000000',
+        },
+      },
+    },
   },
-  plugins: [],
 };
