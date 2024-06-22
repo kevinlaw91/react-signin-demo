36f521ab6f0913cc5bd65cf03baacf1c81e6c51c
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2024-06-22 21:02:26 +0800

    Added jsdom

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
@@ -13,4 +14,7 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+  },
 });
