cd980c0a92a156d6b325c36869aa1be42db4c349
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2024-06-30 18:27:50 +0800

    feat: when signed in block sign up page access

--- src/routes/SignUpPage.tsx
+++ src/routes/SignUpPage.tsx
@@ -1,6 +1,19 @@
+import { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
+import AuthContext from '@/context/AuthContext.tsx';
 
 export default function SignUpPage() {
+  const navigate = useNavigate();
+  const { activeUser, setActiveUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    // If already signed in, redirect to home
+    if (activeUser) {
+      navigate('/', { replace: true });
+    }
+  }, [activeUser, navigate]);
+
   return (
     <>
       <Helmet>
