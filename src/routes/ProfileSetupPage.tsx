b40c3604e735dedb345b98f2803dc61bf38f7945
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2024-07-15 03:33:08 +0800

    refactor: refactor form logics for profile setup page

--- src/routes/ProfileSetupPage.tsx
+++ src/routes/ProfileSetupPage.tsx
@@ -17,7 +17,8 @@ import {
   ERR_USERNAME_TAKEN,
 } from '@/services/profile.ts';
 
-const userNameSchema = z.object({
+// Username awaiting availability check
+const usernameSchema = z.object({
   username: z.string()
     .min(1, { message: 'Please enter a username' })
     .min(6, { message: 'Username needs to be at least 6 characters long' })
@@ -25,7 +26,14 @@ const userNameSchema = z.object({
     .regex(/^[a-z0-9_]*$/, { message: 'Username can only contain letters (a-z), numbers (0-9), and underscores (_)' }),
 });
 
-type UsernameFormData = z.infer<typeof userNameSchema>;
+// Unclaimed username
+const candidateSchema = z.object({
+  candidate: z.string()
+    .min(1, { message: 'Please enter a username' }),
+});
+
+type UsernameFormData = z.infer<typeof usernameSchema>;
+type CandidateFormData = z.infer<typeof candidateSchema>;
 
 const MSG_USERNAME_AVAILABLE = 'Username is available';
 const MSG_USERNAME_ALREADY_TAKEN = 'Username is already taken';
@@ -100,91 +108,39 @@ function mockIsAvailable(username: string) {
   return username.length % 2 === 0;
 }
 
+function UsernameCheckResultMessage({ isAvailable }: { isAvailable: boolean }) {
+  return (
+    <div className="flex gap-2 my-2 items-center">
+      <div className="flex justify-center ">
+        {isAvailable ? animatedTick : animatedError}
+      </div>
+      <div className={clsx('text-xs font-semibold', isAvailable ? 'text-lime-600' : 'text-red-500')}>
+        {isAvailable ? MSG_USERNAME_AVAILABLE : MSG_USERNAME_ALREADY_TAKEN}
+      </div>
+    </div>
+  );
+}
+
 export default function ProfileSetupPage() {
-  const {
-    register,
-    setValue,
-    getValues,
-    setFocus,
-    watch,
-    handleSubmit,
-    formState: {
-      isSubmitSuccessful,
-      isSubmitting,
-    },
-  } = useForm<UsernameFormData>({
-    resolver: zodResolver(userNameSchema),
+  const [isAvailable, setIsAvailable] = useState<boolean | null>(null);
+  const runningUsernameCheck = useRef<AbortController | null>(null);
+
+  const frmCheckUsername = useForm<UsernameFormData>({
+    resolver: zodResolver(usernameSchema),
   });
 
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isAlertModalOpen, setIsAlertModalOpen] = useState(false);
-  const [alertModalMessage, setAlertModalMessage] = useState<string>('');
-  const [isAvailable, setIsAvailable] = useState<boolean | null>(null);
-  const [isValidating, setIsValidating] = useState(false);
-  const runningCheck = useRef<AbortController | null>(null);
+  const frmClaimUsername = useForm<CandidateFormData>({
+    resolver: zodResolver(candidateSchema),
+  });
 
-  const claimUsername = useCallback((profileId: string, username: string) => {
-    const _isAvailable = mockIsAvailable(username);
-
-    // Mock request response
-    fetchMock.patch(`path:/api/profile/${profileId}`,
-      {
-        status: _isAvailable ? 200 : 409,
-        body: _isAvailable
-          ? {
-              success: true,
-              data: {
-                id: profileId,
-                username: username,
-              },
-            }
-          : {
-              success: false,
-            },
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      },
-      { delay: 1000 },
-    );
-
-    // Claim the username
-    return setUsername({ profileId, username })
-      .then((res) => {
-        if (res.success) {
-          alert('ok');
-          return;
-        }
-        throw new Error(ERR_UNEXPECTED_ERROR);
-      })
-      .catch((err) => {
-        if (err instanceof Error) {
-          if (err.message === ERR_USERNAME_TAKEN) {
-            setAlertModalMessage(MSG_USERNAME_CLAIM_FAILED);
-          } else {
-            setAlertModalMessage(MSG_UNEXPECTED_ERROR);
-          }
-          setIsModalOpen(true);
-          setIsAlertModalOpen(true);
-        }
-        throw err;
-      })
-      .finally(() => {
-        // Restore fetch mock
-        fetchMock.restore();
-      });
-  }, []);
-
-  const checkUsername = useCallback(() => {
+  const doCheckUsername = useCallback(async ({ username }: UsernameFormData) => {
     // Discard any unfinished checks
-    runningCheck.current?.abort?.();
-    runningCheck.current = new AbortController();
+    runningUsernameCheck.current?.abort?.();
+    runningUsernameCheck.current = new AbortController();
 
     // Hide errors and display loading spinner
     setIsAvailable(null);
-    setIsValidating(true);
-
-    const username = getValues('username');
+    frmClaimUsername.setValue('candidate', '');
 
     // Mock request response
     fetchMock.get(
@@ -209,48 +165,35 @@ export default function ProfileSetupPage() {
       { delay: 1000 },
     );
 
-    return checkUsernameAvailability(username, runningCheck.current.signal)
-      .then((res) => {
-        const _isAvailable = res?.data?.isAvailable;
-        setIsAvailable(_isAvailable);
+    let _isAvailable;
+    try {
+      const res = await checkUsernameAvailability(username, runningUsernameCheck.current.signal);
+      _isAvailable = res.data.isAvailable;
+    } catch (err) {
+      // Aborted requests are not error
+      if (err instanceof Error && err.name === 'AbortError') return;
 
-        // Reselect input field if username is not available
-        if (!_isAvailable) {
-          setFocus('username');
-        }
-
-        return _isAvailable;
-      })
-      .catch((err) => {
-        if (err instanceof Error && err.name === 'AbortError') {
-          // Request is cancelled intentionally
-        } else {
-          setAlertModalMessage(MSG_UNEXPECTED_ERROR);
-          setIsModalOpen(true);
-          setIsAlertModalOpen(true);
-        }
-        throw err;
-      })
-      .finally(() => {
-        // Hide spinner
-        setIsValidating(false);
-        // Restore fetch mock
-        fetchMock.restore();
-      });
-  }, [getValues, setFocus]);
-
-  const submitHandler: SubmitHandler<UsernameFormData> = useCallback((data: UsernameFormData) => {
-    const profileId = '1234';
-
-    // Make sure username availability check is passed before claiming it
-    if (isAvailable) {
-      return claimUsername(profileId, data.username);
+      setAlertModalMessage(MSG_UNEXPECTED_ERROR);
+      setIsModalOpen(true);
+      setIsAlertModalOpen(true);
+      throw err;
+    } finally {
+      // Restore fetch mock
+      fetchMock.restore();
     }
 
-    throw new Error(ERR_USERNAME_TAKEN);
-  }, [isAvailable, claimUsername]);
+    if (_isAvailable) {
+      frmClaimUsername.setValue('candidate', username);
+    } else {
+      // Reselect input field if username is not available
+      frmCheckUsername.setFocus('username');
+      frmClaimUsername.setValue('candidate', '');
+    }
+    setIsAvailable(_isAvailable);
+    return _isAvailable;
+  }, [frmCheckUsername, frmClaimUsername]);
 
-  const validationFailedHandler: SubmitErrorHandler<UsernameFormData> = useCallback((errors) => {
+  const frmUsernameValidationFailedHandler: SubmitErrorHandler<UsernameFormData> = useCallback((errors) => {
     // Show error dialog if field is blank
     if (errors.username) {
       setAlertModalMessage(errors?.username?.message || '');
@@ -263,13 +206,87 @@ export default function ProfileSetupPage() {
     ref: setUsernameInputRef, // Needed by setFocus()
     onChange: _, // Not used by IMaskInput
     ...otherFieldProps
-  } = register('username');
+  } = frmCheckUsername.register('username');
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isAlertModalOpen, setIsAlertModalOpen] = useState(false);
+  const [alertModalMessage, setAlertModalMessage] = useState<string>('');
+
+  const doClaimUsername: SubmitHandler<CandidateFormData> = useCallback(async (data: CandidateFormData) => {
+    const profileId = '1234';
+    const _isAvailable = mockIsAvailable(data.candidate);
+
+    // Mock request response
+    fetchMock.patch(`path:/api/profile/${profileId}`,
+      {
+        status: _isAvailable ? 200 : 409,
+        body: _isAvailable
+          ? {
+              success: true,
+              data: {
+                id: profileId,
+                username: data.candidate,
+              },
+            }
+          : {
+              success: false,
+            },
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+      { delay: 1000 },
+    );
+
+    // Claim the username
+    let res;
+    try {
+      res = await setUsername({ profileId, username: data.candidate });
+    } catch (err) {
+      if (err instanceof Error) {
+        if (err.message === ERR_USERNAME_TAKEN) {
+          setAlertModalMessage(MSG_USERNAME_CLAIM_FAILED);
+        } else {
+          setAlertModalMessage(MSG_UNEXPECTED_ERROR);
+        }
+        setIsModalOpen(true);
+        setIsAlertModalOpen(true);
+      }
+    } finally {
+      // Restore fetch mock
+      fetchMock.restore();
+    }
+
+    // Success
+    if (res?.success) {
+      return res.data.username;
+    } else {
+      setAlertModalMessage(MSG_UNEXPECTED_ERROR);
+      setIsModalOpen(true);
+      setIsAlertModalOpen(true);
+      throw new Error(ERR_UNEXPECTED_ERROR);
+    }
+  }, []);
+
+  const frmCandidateValidationFailedHandler: SubmitErrorHandler<UsernameFormData> = useCallback((errors) => {
+    // Show error dialog if field is blank
+    if (errors.username) {
+      setAlertModalMessage(errors?.username?.message || '');
+      setIsModalOpen(true);
+      setIsAlertModalOpen(true);
+    }
+  }, []);
 
   const handleAlertModalDismiss = () => {
     setIsModalOpen(false);
     setIsAlertModalOpen(false);
   };
 
+  const formAction = isAvailable
+    ? frmClaimUsername.handleSubmit(doClaimUsername, frmCandidateValidationFailedHandler)
+    : frmCheckUsername.handleSubmit(doCheckUsername, frmUsernameValidationFailedHandler);
+
+  const isSubmitting = frmClaimUsername.formState.isSubmitting || frmClaimUsername.formState.isSubmitting;
   return (
     <>
       <Helmet>
@@ -277,7 +294,8 @@ export default function ProfileSetupPage() {
       </Helmet>
       <section className="min-h-svh min-w-svw bg-neutral-100 bg-[url('/assets/images/bg-gradient-subtle-light.jpg')] bg-cover bg-no-repeat">
         <div className="flex justify-center items-center min-h-svh mx-auto px-4 py-12 max-w-md md:max-w-sm md:px-0 md:w-96 sm:px-4">
-          <form onSubmit={handleSubmit(submitHandler, validationFailedHandler)}>
+          <section>
+
             <div className="py-6">
               <img
                 src="/assets/svg/logo.svg"
@@ -289,69 +307,68 @@ export default function ProfileSetupPage() {
             <div className="text-neutral-400 text-xs text-center my-2" role="note">
               You can use letters (a-z), numbers (0-9), and underscores (_)
             </div>
-            <div className="mt-6">
-              <IMaskInput
-                inputRef={setUsernameInputRef}
-                mask={/^[a-zA-Z0-9_]*$/}
-                className="block w-full h-12 px-4 py-2 text-neutral-700 text-center rounded-lg appearance-none bg-neutral-200 placeholder-neutral-400 outline-none focus:ring-1 focus:ring-neutral-400 ring-offset-0 sm:text-sm transition duration-150"
-                maxLength={32}
-                prepareChar={str => str.trim().toLowerCase()}
-                onAccept={(value) => {
-                  setIsAvailable(null);
-                  setValue('username', value);
-                }}
-                defaultValue=""
-                autoFocus
-                placeholder="Enter a username..."
-                value={watch('username')}
-                {...otherFieldProps}
-              />
-            </div>
-            {
-              isValidating && (
-                <div className="flex gap-2 my-2 items-center">
-                  <div className="flex justify-center ">
-                    <LoaderPulsingDotsCircular className="text-lime-600 size-6" />
-                  </div>
-                  <div className="text-neutral-500 text-xs">
-                    Checking availability...
-                  </div>
-                </div>
-              )
-            }
-            {
-              isAvailable !== null && (
-                <div className="flex gap-2 my-2 items-center">
-                  <div className="flex justify-center ">
-                    {isAvailable ? animatedTick : animatedError}
-                  </div>
-                  <div className={clsx('text-xs font-semibold', isAvailable ? 'text-lime-600' : 'text-red-500')}>
-                    {isAvailable ? MSG_USERNAME_AVAILABLE : MSG_USERNAME_ALREADY_TAKEN}
-                  </div>
-                </div>
-              )
-            }
-            <div className="pt-3">
-              { (isValidating || isSubmitting) && <ButtonBusy className="w-full" /> }
+            <form onSubmit={formAction}>
+              <div className="mt-6">
+                <IMaskInput
+                  inputRef={setUsernameInputRef}
+                  mask={/^[a-zA-Z0-9_]*$/}
+                  className="block w-full h-12 px-4 py-2 text-neutral-700 text-center rounded-lg appearance-none bg-neutral-200 placeholder-neutral-400 outline-none focus:ring-1 focus:ring-neutral-400 ring-offset-0 sm:text-sm transition duration-150"
+                  maxLength={32}
+                  prepareChar={str => str.trim().toLowerCase()}
+                  onAccept={(value) => {
+                    setIsAvailable(null);
+                    frmCheckUsername.setValue('username', value);
+                    frmClaimUsername.setValue('candidate', '');
+                  }}
+                  defaultValue=""
+                  autoFocus
+                  placeholder="Enter a username..."
+                  value={frmCheckUsername.watch('username')}
+                  {...otherFieldProps}
+                />
+              </div>
               {
-                !isValidating && (isAvailable === null || !isAvailable) && (
-                  <ButtonPrimary type="button" className="w-full" onClick={checkUsername}>Check Availability</ButtonPrimary>
+                frmCheckUsername.formState.isSubmitting && (
+                  <div className="flex gap-2 my-2 items-center">
+                    <div className="flex justify-center ">
+                      <LoaderPulsingDotsCircular className="text-lime-600 size-6" />
+                    </div>
+                    <div className="text-neutral-500 text-xs">
+                      Checking availability...
+                    </div>
+                  </div>
                 )
               }
-              <AnimatePresence>
+              {isAvailable !== null && <UsernameCheckResultMessage isAvailable={isAvailable} />}
+              <div className="pt-3">
                 {
-                  isAvailable && (!isSubmitting && !isSubmitSuccessful) && (
-                    <motion.div
-                      initial={{ opacity: 0, y: 50 }}
-                      animate={{ opacity: 1, y: 0 }}
-                    >
-                      <ButtonPrimary type="submit" className="w-full">Confirm</ButtonPrimary>
-                    </motion.div>
+                  isSubmitting
+                  && <ButtonBusy className="w-full" />
+                }
+                {
+                  !frmCheckUsername.formState.isSubmitting && (isAvailable === null || !isAvailable) && (
+                    <ButtonPrimary type="submit" className="w-full">Check Availability</ButtonPrimary>
                   )
                 }
-              </AnimatePresence>
-            </div>
-          </form>
+                {
+                  (!frmClaimUsername.formState.isSubmitSuccessful && !frmClaimUsername.formState.isSubmitting && isAvailable === true)
+                  && (
+                    <AnimatePresence>
+                      (
+                      isAvailable === true &&
+                      <motion.div
+                        initial={{ opacity: 0, y: 50 }}
+                        animate={{ opacity: 1, y: 0 }}
+                      >
+                        <ButtonPrimary type="submit" className="w-full">Confirm</ButtonPrimary>
+                      </motion.div>
+                      )
+                    </AnimatePresence>
+                  )
+                }
+              </div>
+            </form>
+          </section>
         </div>
       </section>
       <AnimatePresence>
