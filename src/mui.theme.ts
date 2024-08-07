e29914770f44da3f73f792a7256c525cefe40ba6
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2024-08-08 02:07:40 +0800

    feat: integrate tailwind theme into MUI theme

--- src/mui.theme.ts
+++ src/mui.theme.ts
@@ -1,13 +1,28 @@
 import { createTheme } from '@mui/material/styles';
-import { colors } from '../tailwind.config.js';
+import resolveConfig from 'tailwindcss/resolveConfig';
+import tailwindConfigFile from '../tailwind.config.js';
 
 interface TailwindColor { [key: string]: string }
 interface TailwindColors { [key: string]: TailwindColor }
 
+const tailwindConfig = resolveConfig(tailwindConfigFile);
+const twColors: TailwindColors = tailwindConfig.theme.colors as unknown as TailwindColors;
+
+// Integrate tailwind theme into MUI theme
 export const theme = createTheme({
   palette: {
     primary: {
-      main: (colors as TailwindColors).primary.DEFAULT,
+      main: twColors.primary.DEFAULT,
+      light: twColors.primary['400'],
+      dark: twColors.primary['800'],
     },
+    secondary: {
+      main: twColors.secondary.DEFAULT,
+      light: twColors.secondary['400'],
+      dark: twColors.secondary['800'],
+    },
+  },
+  typography: {
+    fontFamily: tailwindConfig.theme.fontFamily['sans'].join(','),
   },
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,35 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-export const colors = {
-  primary: {
-    DEFAULT: '#f13f0a',
-    50: '#fff5ed',
-    100: '#ffe8d5',
-    200: '#ffcda9',
-    300: '#feaa73',
-    400: '#fd7c3a',
-    500: '#fb5914',
-    600: '#f13f0a',
-    700: '#c42b0a',
-    800: '#9b2311',
-    900: '#7d2011',
-    950: '#440d06',
-  },
-  secondary: {
-    DEFAULT: '#00EFFA',
-    50: '#C9FFF7',
-    100: '#B3FFF6',
-    200: '#85FFF5',
-    300: '#57FFF9',
-    400: '#29FEFF',
-    500: '#00EFFA',
-    600: '#00B6C7',
-    700: '#008194',
-    800: '#005061',
-    900: '#00242E',
-    950: '#001014',
-  },
-};
-
 export default {
   content: [
     './index.html',
@@ -37,7 +6,36 @@ export default {
   ],
   theme: {
     extend: {
-      colors: colors,
+      colors: {
+        primary: {
+          DEFAULT: '#f13f0a',
+          50: '#fff5ed',
+          100: '#ffe8d5',
+          200: '#ffcda9',
+          300: '#feaa73',
+          400: '#fd7c3a',
+          500: '#fb5914',
+          600: '#f13f0a',
+          700: '#c42b0a',
+          800: '#9b2311',
+          900: '#7d2011',
+          950: '#440d06',
+        },
+        secondary: {
+          DEFAULT: '#00EFFA',
+          50: '#C9FFF7',
+          100: '#B3FFF6',
+          200: '#85FFF5',
+          300: '#57FFF9',
+          400: '#29FEFF',
+          500: '#00EFFA',
+          600: '#00B6C7',
+          700: '#008194',
+          800: '#005061',
+          900: '#00242E',
+          950: '#001014',
+        },
+      },
     },
   },
 };
