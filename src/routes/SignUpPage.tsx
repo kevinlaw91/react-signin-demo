d7fe5b48497d13330f9d5e771e2c6bba1866782e
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2024-08-11 21:19:56 +0800

    refactor: migrate signup page alerts to useAlertPopupModal

--- src/routes/SignUpPage.tsx
+++ src/routes/SignUpPage.tsx
@@ -1,20 +1,19 @@
 import { useCallback, useContext, useEffect, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import { AnimatePresence, motion } from 'framer-motion';
+import { AnimatePresence } from 'framer-motion';
 import { Helmet } from 'react-helmet-async';
 import AuthSignUpForm from '@/components/AuthSignUpForm.tsx';
 import BusyScreen from '@/components/BusyScreen.tsx';
 import AuthContext, { AuthenticatedUser } from '@/contexts/AuthContext.tsx';
-import AlertModal from '@/components/AlertModal.tsx';
+import { useAlertPopupModal } from '@/hooks/useAlertPopupModal.ts';
 import GoogleSignInButton from '@/components/GoogleSignInButton.tsx';
 
 export default function SignUpPage() {
   const navigate = useNavigate();
   const { activeUser, setActiveUser } = useContext(AuthContext);
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isAlertModalOpen, setIsAlertModalOpen] = useState(false);
-  const [alertModalMessage, setAlertModalMessage] = useState<string | undefined>();
+
+  const { queueAlertModal } = useAlertPopupModal();
 
   useEffect(() => {
     // If already signed in, redirect to home
@@ -24,12 +23,10 @@ export default function SignUpPage() {
   }, [activeUser, navigate]);
 
   const onFormSignUpSubmit = () => {
-    setIsModalOpen(true);
     setIsSubmitting(true);
   };
 
   const onFormSignUpSuccess = useCallback((user: AuthenticatedUser) => {
-    setIsModalOpen(false);
     setIsSubmitting(false);
 
     // Change app's state to signed in
@@ -40,15 +37,8 @@ export default function SignUpPage() {
   }, [setActiveUser, navigate]);
 
   const onFormSignUpError = (err?: string) => {
-    setIsModalOpen(true);
     setIsSubmitting(false);
-    setIsAlertModalOpen(true);
-    setAlertModalMessage(err || 'Sign up failed');
-  };
-
-  const handleAlertModalDismiss = () => {
-    setIsModalOpen(false);
-    setIsAlertModalOpen(false);
+    queueAlertModal(err || 'Sign up failed');
   };
 
   return (
@@ -95,26 +85,7 @@ export default function SignUpPage() {
         <div className="row-span-full col-span-full bg-[url('/assets/images/pawel-czerwinski-Zd315A95aqg-unsplash.webp')] bg-cover bg-center bg-no-repeat"></div>
       </div>
       <AnimatePresence>
-        {isModalOpen && (
-          <motion.div
-            initial={{ backdropFilter: 'blur(0px)', opacity: 0 }}
-            animate={{ backdropFilter: 'blur(10px)', opacity: 1 }}
-            exit={{ backdropFilter: 'blur(0px)', opacity: 0 }}
-            transition={{ duration: 0.25 }}
-            className="fixed inset-0 bg-black/60"
-          />
-        )}
-      </AnimatePresence>
-      <AnimatePresence>
         {isSubmitting && <BusyScreen message="Creating account" messageClassName="text-white/90" />}
-      </AnimatePresence>
-      <AnimatePresence>
-        {isAlertModalOpen && (
-          <AlertModal
-            message={alertModalMessage}
-            dismiss={handleAlertModalDismiss}
-          />
-        )}
       </AnimatePresence>
     </>
   );
