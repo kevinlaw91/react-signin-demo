1f5a04fc7d89be9f4a54d8492e398b60e72f9c5a
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2024-08-15 15:24:37 +0800

    feat: update style of profile setup complete page

--- src/routes/ProfileSetupComplete.tsx
+++ src/routes/ProfileSetupComplete.tsx
@@ -7,10 +7,10 @@ export default function ProfileSetupComplete() {
   return (
     <section>
       <div className="flex h-full min-h-svh flex-col items-center justify-center py-12 gap-12">
-        <div className="flex flex-col items-center justify-center">
-          <img src="/assets/svg/success-circle-large.svg" className="size-3/5" />
-          <h1 className="text-2xl font-bold">Complete</h1>
-          <p>You profile is now set up.</p>
+        <div className="flex flex-col items-center justify-center text-center">
+          <img src="/assets/svg/success-circle-large.svg" className="size-3/5 mb-3.5" alt="" />
+          <h1 className="text-2xl font-bold text-primary-500">Complete</h1>
+          <p className="text-neutral-600 my-3">Your profile is now set up</p>
         </div>
         <div>
           <ButtonPrimary onClick={handleClick}>Go to Home</ButtonPrimary>
