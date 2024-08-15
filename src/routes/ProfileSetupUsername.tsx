kevinlaw91/react-signin-demo

commit 97759c841ce651ab992430c4bba6192ddd701412

fix: handle http error for checkUsernameAvailability

src/routes/ProfileSetupUsername.tsx
@@ -172,29 +172,31 @@ export default function ProfileSetupPage() {
       { delay: 1000 },
     );
 
-    let _isAvailable;
-    try {
-      const res = await checkUsernameAvailability(username, runningUsernameCheck.current.signal);
-      _isAvailable = res.data.isAvailable;
-    } catch (err) {
-      // Aborted requests are not error
-      if (err instanceof Error && err.name === 'AbortError') return;
-      queueAlertModal(MSG_UNEXPECTED_ERROR);
-      throw err;
-    } finally {
-      // Restore fetch mock
-      fetchMock.restore();
-    }
+    checkUsernameAvailability(username, runningUsernameCheck.current.signal)
+      .then((res) => {
+        const available = res.data.isAvailable;
 
-    if (_isAvailable) {
-      frmClaimUsername.setValue('candidate', username);
-    } else {
-      // Reselect input field if username is not available
-      frmCheckUsername.setFocus('username');
-      frmClaimUsername.setValue('candidate', '');
-    }
-    setIsAvailable(_isAvailable);
-    return _isAvailable;
+        if (available) {
+          frmClaimUsername.setValue('candidate', username);
+        } else {
+          // Reselect input field if username is not available
+          frmCheckUsername.setFocus('username');
+          frmClaimUsername.setValue('candidate', '');
+        }
+
+        setIsAvailable(available);
+        return available;
+      })
+      .catch((err) => {
+        // Aborted requests are not error
+        if (err instanceof Error && err.name === 'AbortError') return;
+        console.error(err);
+        queueAlertModal(MSG_UNEXPECTED_ERROR);
+      })
+      .finally(() => {
+        // Restore fetch mock
+        fetchMock.restore();
+      });
   }, [frmCheckUsername, frmClaimUsername, queueAlertModal]);
 
   const frmUsernameValidationFailedHandler: SubmitErrorHandler<UsernameFormData> = useCallback((errors) => {

src/services/profile.ts
@@ -55,7 +55,12 @@ export async function checkUsernameAvailability(username: string, signal?: Abort
     method: 'GET',
     headers: { 'Content-Type': 'application/json' },
   });
-  return await response.json() as CheckUsernameResponse;
+
+  if (response.ok) {
+    return await response.json() as CheckUsernameResponse;
+  }
+
+  throw new Error(`${response.statusText} ${response.status}`);
 }
 
 interface UploadProfilePictureSuccessResponse {

commit ed9f891f00778047be30bdbabd753ce6e0f0ad0a

fix: loading indicator not shown when checking username availability. bug introduced by 97759c8

src/routes/ProfileSetupUsername.tsx
@@ -175,7 +175,7 @@ export default function ProfileSetupUsername() {
       { delay: 1000 },
     );
 
-    checkUsernameAvailability(username, runningUsernameCheck.current.signal)
+    return checkUsernameAvailability(username, runningUsernameCheck.current.signal)
       .then((res) => {
         const available = res.data.isAvailable;
 
@@ -188,7 +188,7 @@ export default function ProfileSetupUsername() {
         }
 
         setIsAvailable(available);
-        return available;
+        return;
       })
       .catch((err) => {
         // Aborted requests are not error