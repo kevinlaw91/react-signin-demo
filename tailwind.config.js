a69feb747da6b34312025f3a9f04a4ef4cc78a7f
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2024-08-19 12:29:01 +0800

    fix: type annotation error for tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
+/** @type {import('tailwindcss').Config} */
 import defaultTheme from 'tailwindcss/defaultTheme';
 
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './index.html',
