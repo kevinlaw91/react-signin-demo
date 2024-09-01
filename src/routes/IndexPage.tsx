a26dc2c74edbed586a505cab1bfefbed5c66a697
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2024-09-01 15:36:27 +0800

    feat: added main section to index page

--- src/routes/IndexPage.tsx
+++ src/routes/IndexPage.tsx
@@ -6,6 +6,7 @@ import { Button } from '@/components/Button.tsx';
 import { Drawer, IconButton } from '@mui/material';
 import { Icon } from '@iconify-icon/react';
 import SidebarMenu from '@/components/SidebarMenu.tsx';
+import { motion } from 'framer-motion';
 
 function UserWelcomeScreen() {
   const { user } = useContext(SessionContext);
@@ -20,8 +21,8 @@ function UserWelcomeScreen() {
   const profileLink = `/profile/${user.id}`;
 
   return (
-    <section className="min-h-full min-w-[320px] bg-neutral-50">
-      <section className="flex content-start flex-wrap justify-end bg-gradient-to-r from-orange-300 to-rose-300 rounded-b-2xl h-32">
+    <section className="min-h-svh min-w-[320px] bg-gradient-to-b from-neutral-50 to-amber-50">
+      <section className="flex content-start flex-wrap justify-end bg-gradient-to-r from-orange-800 via-orange-500 to-amber-500 rounded-b-2xl h-32">
         <IconButton aria-label="Menu" size="large" onClick={toggleDrawer(true)}>
           <Icon icon="solar:menu-dots-bold" className="text-white" />
         </IconButton>
@@ -56,6 +57,45 @@ function UserWelcomeScreen() {
             <span className="text-sm md:text-lg">Welcome,&nbsp;</span>
             <Link to={profileLink} className="text-md md:text-lg font-semibold hover:text-primary">{user.username || user.id}</Link>
           </h1>
+        </div>
+      </section>
+      <section className="mx-2 md:mx-8">
+        <section className="relative h-[300px] my-6 px-8 py-12 rounded-lg shadow-lg bg-gradient-to-tr from-neutral-800 via-neutral-950 via-70% to-orange-950 overflow-hidden">
+          <div className="h-[100px] w-[100px] border-2 border-white/10 absolute right-[5%] -bottom-[30px] rotate-45" role="presentation"></div>
+          <h2 className="font-bold text-3xl text-white">Basic Plan</h2>
+          <div className="my-6">
+            <Button className="text-white hover:text-white text-xs border border-white/20 hover:border-white/50">Upgrade</Button>
+          </div>
+          <div className="my-6">
+            <div className="h-[1px] bg-gradient-to-r from-neutral-200/30 to-neutral-200/10" />
+          </div>
+        </section>
+      </section>
+      <section className="mt-16 mb-24 mx-2 md:mx-8">
+        <h2 className="inline-block font-bold text-3xl bg-clip-text text-transparent bg-gradient-to-r from-amber-800 to-orange-600 px-6">Discover</h2>
+        <motion.div
+          variants={{
+            hidden: { opacity: 0 },
+            visible: { opacity: 1, transition: { staggerChildren: 0.4 } },
+          }}
+          initial="hidden"
+          animate="visible"
+          whileInView="visible"
+          viewport={{ once: true }}
+          className="my-6 grid auto-rows-[260px] grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4"
+        >
+          {Array(4).fill(0).map((_, index) => (
+            <motion.div
+              key={index}
+              variants={{
+                hidden: { opacity: 0, y: 25 },
+                visible: { opacity: 1, y: 0, transition: { duration: 1 } },
+              }}
+              className="bg-neutral-200 rounded-lg"
+            />
+          ))}
+        </motion.div>
+      </section>
         </div>
       </section>
     </section>
