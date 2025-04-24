b62a8722fb6fc652c1fee500b6fa212639c7787a
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2025-04-24 15:49:02 +0800

    fix: react-router warning of missing HydrateFallback

--- src/router.tsx
+++ src/router.tsx
@@ -1,27 +1,35 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+// Show nothing during hydration
+const hydrateFallbackComponent = () => null;
+
 const router = createBrowserRouter(
   [
     {
       path: '/',
       lazy: () => import('@/routes/IndexPage'),
+      HydrateFallback: hydrateFallbackComponent,
     },
     {
       path: '/signin',
       lazy: () => import('@/routes/SignInPage'),
+      HydrateFallback: hydrateFallbackComponent,
     },
     {
       path: '/signup',
       lazy: () => import('@/routes/SignUpPage'),
+      HydrateFallback: hydrateFallbackComponent,
     },
     {
       path: '/setup',
       lazy: () => import('@/routes/ProfileSetupPage'),
+      HydrateFallback: hydrateFallbackComponent,
     },
     {
       // 404 page
       path: '*',
       lazy: () => import('@/routes/NotFoundPage'),
+      HydrateFallback: hydrateFallbackComponent,
     },
   ],
   {
