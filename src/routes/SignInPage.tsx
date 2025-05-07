ed205908497b0f8aebca3de9f29bd3547f7ab9b3
kevinlaw91 <kevinlaw91@users.noreply.github.com>
2025-05-07 18:04:37 +0800

    feat: tweak button focus outline color

--- src/routes/SignInPage.tsx
+++ src/routes/SignInPage.tsx
@@ -95,7 +95,7 @@ export function Component() {
             <div className="text-center mt-3">
               <Link
                 to="/signup"
-                className="text-sm text-neutral-800 font-semibold hover:text-neutral-500 px-6 py-2 outline-hidden focus:ring-2 ring-primary ring-offset-0 rounded-sm"
+                className="text-sm text-neutral-800 font-semibold hover:text-neutral-500 px-6 py-2 outline-hidden focus:ring-2 ring-neutral-300 ring-offset-0 rounded-sm"
               >
                 Create Account
               </Link>
